4ffc3fd3770d7c66216b781129cd68ff1c5b7934
ahmed hameed <ahmed1.hameed@gmail.com>
2024-08-26 16:09:19 -0400

    first commit

---
+++ backend/src/test/java/com/example/SBProductAPIDemo/SbProductApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBProductAPIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbProductApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
